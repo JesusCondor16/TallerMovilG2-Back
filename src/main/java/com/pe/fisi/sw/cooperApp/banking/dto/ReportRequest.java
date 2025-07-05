c7edd5586492f09b262105edf7baf19206ca835d
JoaquinHC9 <joaquin.hidalgo@unmsm.edu.pe>
2025-07-05 05:27:42 -0500

    add

--- src/main/java/com/pe/fisi/sw/cooperApp/banking/dto/ReportRequest.java
+++ src/main/java/com/pe/fisi/sw/cooperApp/banking/dto/ReportRequest.java
@@ -2,9 +2,11 @@ package com.pe.fisi.sw.cooperApp.banking.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ReportRequest {
     private String cuentaUid;
     private String reporterId;
